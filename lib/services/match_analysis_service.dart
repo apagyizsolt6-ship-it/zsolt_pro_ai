3a3be3530f31385eb951a50d237879e141482a82
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-28 18:34:11 +0200

    Update match_analysis_service.dart

--- lib/services/match_analysis_service.dart
+++ lib/services/match_analysis_service.dart
@@ -199,7 +199,7 @@ class MatchAnalysisService {
     if (_oddsService.hasApiKey) {
       try {
         final OddsEvent? event = await _oddsService.findMatchOdds(
-          sportKey: match.sport,
+          sportKey: match.sportKey,
           homeTeam: match.homeTeam,
           awayTeam: match.awayTeam,
           matchDate: match.matchDate,
@@ -262,8 +262,8 @@ class MatchAnalysisResult {
   double get recommendationProbability => analysis.recommendation.probability;
   
   double get fairOdds => analysis.recommendation.fairOdds;
-  double get marketOdds => analysis.recommendation.price;
-  double get valueEdgePercentage => analysis.recommendation.valueEdge;
+  double get marketOdds => analysis.recommendation.odds;
+  double get valueEdgePercentage => analysis.recommendation.edge;
 
   String get dataSourceLabel => statisticsResult.sourceLabel;
   String get qualityLabel => statisticsResult.qualityLabel;
